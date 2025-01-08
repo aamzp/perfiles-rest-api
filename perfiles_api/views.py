32e00210556d6f68253d01be167f711595ae64fb
AdolfoMaza <amazap@amazap.cl>
2025-01-08 15:21:52 -0300

    ApiView y testeo inicial

--- perfiles_api/views.py
+++ perfiles_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class HelloApiView(APIView):
+    """Testeando API View"""
+
+    def get(self, request, format=None):
+        """Retornar una lista de atributos APIView, algo típico de un get"""
+
+        an_apiview = [
+            'métodos HTTP (get, post, patch, put, delete)',
+            'Es similar a una vista tradicional de Django',
+            'Nos da el mayor control sobre la lógica de nuestra aplicación',
+            'Está mapeado manualmente a los URLs',
+            
+
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
